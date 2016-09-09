5df96e0a97cebb6638da767ae9fafd6e300e87de
svallero <nebula@srm-dom0.to.infn.it>
2016-09-09 18:43:27 +0200

    First commit, basically a placeholder.

---
+++ include/RequestSystem.h
@@ -0,0 +1,62 @@
+/* ------------------------------------ */
+/* HEADER                               */
+/*                                      */
+/* Mailto: svallero AT to.infn.it       */
+/*                                      */
+/* ------------------------------------ */
+
+#ifndef REQUEST_SYSTEM_H
+#define REQUEST_SYSTEM_H
+
+#include "Request.h"
+
+using namespace std;
+
+class RequestSystem: public Request
+{
+protected:
+    RequestSystem( const string& method_name,
+                       const string& help,
+                       const string& params)
+        :Request(method_name,params,help)
+    {};
+
+    ~RequestSystem(){};
+
+
+    virtual void request_execute(xmlrpc_c::paramList const& _paramList,
+                                 RequestAttributes& att) = 0;
+};
+
+class SystemVersion : public RequestSystem
+{
+public:
+    SystemVersion():
+        RequestSystem("SystemVersion",
+                          "Returns the FASS version",
+                          "A:s"){};
+
+    ~SystemVersion(){};
+
+    void request_execute(xmlrpc_c::paramList const& _paramList,
+                         RequestAttributes& att);
+};
+
+/*
+class SystemConfig : public RequestSystem
+{
+public:
+    SystemConfig():
+        RequestSystem("SystemConfig",
+                          "Returns the FASS configuration",
+                          "A:s")
+    {};
+
+    ~SystemConfig(){};
+
+    void request_execute(xmlrpc_c::paramList const& _paramList,
+                         RequestAttributes& att);
+};	
+*/
+
+#endif
